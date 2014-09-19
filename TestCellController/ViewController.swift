6c40b2fde61d712d50965ec2ca7dea1fa73bb68a
James Tang <j@jamztang.com>
2014-09-19 23:33:16 +0800

    Initial Commit

---
+++ TestCellController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCellController
+//
+//  Created by James Tang on 19/9/14.
+//  Copyright (c) 2014 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
